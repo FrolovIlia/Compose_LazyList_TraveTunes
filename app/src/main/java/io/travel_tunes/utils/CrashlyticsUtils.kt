f8ea69527668bf82387aecb9803960b8dbba5042
helton57 <AlexAndreenkov@mail.ru>
2024-04-20 19:17:07 +0300

    update CrashlyticsUtils

--- app/src/main/java/io/travel_tunes/utils/CrashlyticsUtils.kt
+++ app/src/main/java/io/travel_tunes/utils/CrashlyticsUtils.kt
@@ -1,10 +1,12 @@
 package io.travel_tunes.utils
 
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import timber.log.Timber
 class CrashlyticsUtils {
     companion object {
         fun sendThrowableNonFatal(e: Throwable) {
             Timber.e(e)
+            FirebaseCrashlytics.getInstance().recordException(e)
         }
 
         fun sendThrowableNonFatal(string: String) {
