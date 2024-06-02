77a14453588114057e17bacefabaa0878159e0bf
helton57 <AlexAndreenkov@mail.ru>
2024-06-02 20:33:42 +0300

    add sendEmail to RestoreFragment

--- app/src/main/java/io/travel_tunes/ui/fragments/restore/RestoreFragment.kt
+++ app/src/main/java/io/travel_tunes/ui/fragments/restore/RestoreFragment.kt
@@ -11,6 +11,7 @@ import io.travel_tunes.R
 import io.travel_tunes.databinding.FragmentRestoreBinding
 import io.travel_tunes.utils.extencions.changeVisibility
 import io.travel_tunes.utils.extencions.copyToClipboard
+import io.travel_tunes.utils.extencions.openEmailApp
 import io.travel_tunes.utils.prefs.PreferenceManager
 
 class RestoreFragment : Fragment() {
@@ -45,13 +46,16 @@ class RestoreFragment : Fragment() {
         initToolbar()
         with(binding) {
             toolbarLayout.toolbarTitle.setText(R.string.restore_screen_title)
+            // FIXME: возможно стоит выпилить копирование адреса по клику на текстовое поле 
             descriptionContacts.setOnClickListener {
-                // FIXME: также тут можно запускать почту и вставлять наш адрес - возможно это будет лучшим решением
                 requireActivity().copyToClipboard(
                     label = "email",
                     textForCopy = "pixel.rabbit.soft@gmail.com",
                     textForToast = "Е-mail скопирован"
                 )
+            }
+            actionSendMail.setOnClickListener {
+                requireActivity().openEmailApp("pixel.rabbit.soft@gmail.com")
             }
             actionCopyID.setOnClickListener {
                 viewModel.getUID { uniqueId ->
--- app/src/main/java/io/travel_tunes/utils/extencions/Activity.kt
+++ app/src/main/java/io/travel_tunes/utils/extencions/Activity.kt
@@ -5,9 +5,14 @@ import android.content.ClipData
 import android.content.ClipDescription
 import android.content.ClipboardManager
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.os.PersistableBundle
+import android.view.View
 import android.widget.Toast
+import io.travel_tunes.R
+import io.travel_tunes.utils.CrashlyticsUtils
 
 fun Activity.copyToClipboard(
     label: String,
@@ -28,4 +33,23 @@ fun Activity.copyToClipboard(
     // Only show a toast for Android 12 and lower.
     if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.S_V2)
         Toast.makeText(this, textForToast, Toast.LENGTH_SHORT).show()
+}
+
+fun Activity.openEmailApp(emailAddress: String?) {
+    if (emailAddress.isNullOrBlank()) return
+    val context = this
+    val intent = Intent(Intent.ACTION_SENDTO)
+    intent.data =
+        Uri.parse(context.getString(R.string.contacts_mail_link_pattern, emailAddress))
+    try {
+        context.startActivity(
+            Intent.createChooser(
+                intent,
+                context.getString(R.string.contacts_select_mail)
+            )
+        )
+    } catch (th: Throwable) {
+        CrashlyticsUtils.sendThrowableNonFatal(th)
+        Toast.makeText(context, R.string.application_not_found, Toast.LENGTH_SHORT).show()
+    }
 }
