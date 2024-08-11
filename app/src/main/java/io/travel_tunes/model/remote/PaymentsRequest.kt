a02331a225418061e224a6969bbd3ef86ee499b4
helton57 <AlexAndreenkov@mail.ru>
2024-08-11 23:30:42 +0300

    fix SerializedName for capture at createPayment

--- app/src/main/java/io/travel_tunes/model/remote/PaymentsRequest.kt
+++ app/src/main/java/io/travel_tunes/model/remote/PaymentsRequest.kt
@@ -11,5 +11,5 @@ data class PaymentsRequest(
     @Expose @SerializedName("metadata") private val metadata: MetadataRequest,
     @Expose @SerializedName("payment_token") private val paymentToken: String,
     //провести обычный платеж (параметр capture со значением true). Это значит, что сразу после оплаты платеж успешно завершится и перейдет в статус succeeded.
-    @Expose private val capture: Boolean = true
+    @Expose @SerializedName("capture") private val capture: Boolean = true
 ) : Parcelable
