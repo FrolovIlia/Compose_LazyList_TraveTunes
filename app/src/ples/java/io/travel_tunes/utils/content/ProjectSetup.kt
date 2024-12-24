cf803b31ccf7b3556c23c005d4ff4567725fcfaf
FrolovIlia <ilia_frolov@list.ru>
2024-12-24 20:12:57 +0300

    Add Ples city.

---
+++ app/src/ples/java/io/travel_tunes/utils/content/ProjectSetup.kt
@@ -0,0 +1,83 @@
+package io.travel_tunes.utils.content
+
+import io.travel_tunes.R
+import io.travel_tunes.model.payments.PaymentVariant
+import io.travel_tunes.utils.Payments
+import java.util.Currency
+object ProjectSetup {
+    const val TABLE_NAME = "ples_app"
+
+    val ROUTES_LIST = listOf(RoutePles1())
+
+    val PAYMENT_VARIANTS = setOf(
+        PaymentVariant(
+            name = "ples_1_item_key",
+            labelStringRes = R.string.payment_variant_1_label,
+            routeTagItems = setOf(RoutePles1().getRouteTag()),
+            amount = 499.0,
+            currency = Currency.getInstance(Payments.CURRENCY_RUB)
+        )
+    )
+
+    /**
+     * как пример для 2 платежей
+     */
+//    val PAYMENT_VARIANTS = setOf(
+//        PaymentVariant(
+//            name = "kazan_1_item_key",
+//            routeTagItems = setOf("kazan_1"),
+//            amount = "499₽"
+//        ),
+//        PaymentVariant(
+//            name = "kazan_2_item_key",
+//            routeTagItems = setOf("kazan_2"),
+//            amount = "499₽"
+//        ),
+//        PaymentVariant(
+//            name = "kazan_1_2_items_key",
+//            routeTagItems = setOf("kazan_1", "kazan_2"),
+//            amount = "899₽"
+//        )
+//    )
+
+    /**
+     * как пример для 3 платежей
+     */
+//    val PAYMENT_VARIANTS = setOf(
+//        PaymentVariant(
+//            name = "kazan_1_item_key",
+//            routeTagItems = setOf("kazan_1"),
+//            amount = "499₽"
+//        ),
+//        PaymentVariant(
+//            name = "kazan_2_item_key",
+//            routeTagItems = setOf("kazan_2"),
+//            amount = "499₽"
+//        ),
+//        PaymentVariant(
+//            name = "kazan_3_item_key",
+//            routeTagItems = setOf("kazan_3"),
+//            amount = "499₽"
+//        ),
+//        PaymentVariant(
+//            name = "kazan_1_2_items_key",
+//            routeTagItems = setOf("kazan_1", "kazan_2"),
+//            amount = "899₽"
+//        ),
+//        PaymentVariant(
+//            name = "kazan_1_3_items_key",
+//            routeTagItems = setOf("kazan_1", "kazan_3"),
+//            amount = "899₽"
+//        ),
+//        PaymentVariant(
+//            name = "kazan_2_3_items_key",
+//            routeTagItems = setOf("kazan_2", "kazan_3"),
+//            amount = "899₽"
+//        ),
+//        PaymentVariant(
+//            name = "kazan_1_2_3_items_key",
+//            routeTagItems = setOf("kazan_1", "kazan_2", "kazan_3"),
+//            amount = "1199₽"
+//        )
+//    )
+}
