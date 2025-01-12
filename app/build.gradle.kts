0c3f2972e7929f7424b2b827d5b7077199b3fbe8
FrolovIlia <ilia_frolov@list.ru>
2025-01-12 21:05:00 +0300

    Fix part Suzdal content

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,8 +26,8 @@ secrets {
 android {
     namespace = "io.travel_tunes"
     compileSdk = 35
-    val versionName = "1.0.1"
-    val versionCode = 1
+    val versionName = "1.0.2"
+    val versionCode = 2
 
     defaultConfig {
         applicationId = "io.travel_tunes"
@@ -114,15 +114,30 @@ android {
             buildConfigField(
                 "String",
                 "MERCHANT_TOKEN",
-                "\"test_NDA3NTUyT_KH6UhrXdDGpWDUat7BGE7ubpQMdQ2fNA4\""
+                "\"live_MzY4NDI5w03wj0VlTBj1iajKVLO10lme8fpvmzeUaXI\""
             )
             buildConfigField(
                 "String",
                 "SECRET_KEY",
-                "\"test__PHJmGyxUWpFrUuhx8tzfgkaVL0ALfucbqDBzwTmyqo\""
+                "\"live_-UQn7qmMiOQQCOntg9njEvy1w1CalDwZnLoF2m177_g\""
             )
-            buildConfigField("String", "SHOP_ID", "\"407552\"")
+            buildConfigField("String", "SHOP_ID", "\"368429\"")
         }
+//        create("suzdal") {
+//            applicationId = "io.travel_tunes.suzdal"
+//            resValue("string", "app_name", "TravelTunes Суздаль")
+//            buildConfigField(
+//                "String",
+//                "MERCHANT_TOKEN",
+//                "\"test_NDA3NTUyT_KH6UhrXdDGpWDUat7BGE7ubpQMdQ2fNA4\""
+//            )
+//            buildConfigField(
+//                "String",
+//                "SECRET_KEY",
+//                "\"test__PHJmGyxUWpFrUuhx8tzfgkaVL0ALfucbqDBzwTmyqo\""
+//            )
+//            buildConfigField("String", "SHOP_ID", "\"407552\"")
+//        }
 
     }
 
