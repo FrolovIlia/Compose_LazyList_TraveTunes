a1fa52e7a716a8550cf6c1fb487745136b0d4e2e
FrolovIlia <ilia_frolov@list.ru>
2025-01-21 21:30:00 +0300

    Add points description, and fix photo numeration.

--- app/src/ples/java/io/travel_tunes/utils/content/RoutePles1.kt
+++ app/src/ples/java/io/travel_tunes/utils/content/RoutePles1.kt
@@ -12,11 +12,11 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 data class RoutePles1(
     private val tag: String = "ples_1",
-    private val jsonDescription: Int = R.raw.route_suzdal_1,
+    private val jsonDescription: Int = R.raw.route_ples_1,
     //Используем общее изображение, но в данном случае первая была очень невзрачная, так что выбрана что симпотичнее
     private val pictureMainRes: Int = R.drawable.route_ples_1_4,
     //Используем изображение первой точки маршрута в описании маршрута, но в данном случае первая была очень невзрачная, так что выбрана что симпотичнее
-    private val pictureDescriptionRes: Int = R.drawable.route_ples_1_7,
+    private val pictureDescriptionRes: Int = R.drawable.route_ples_1_4,
     private val audioRes: Int = R.raw.route_ples_1_description
 ) : RouteSealedInfo() {
     override fun getRouteTag() = tag.lowercase()
