cea2c7ee7a4c431b2ee8c1b5a74d2c4a184070b4
FrolovIlia <ilia_frolov@list.ru>
2025-02-03 22:30:27 +0300

    Update icons for all apps. Update prices.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -222,7 +222,6 @@ android {
 }
 
 dependencies {
-//    val roomVersion = "2.6.1"
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
--- app/src/kazan/java/io/travel_tunes/utils/content/ProjectSetup.kt
+++ app/src/kazan/java/io/travel_tunes/utils/content/ProjectSetup.kt
@@ -14,7 +14,7 @@ object ProjectSetup {
             name = "kazan_1_item_key",
             labelStringRes = R.string.payment_variant_1_label,
             routeTagItems = setOf(RouteKazan1().getRouteTag()),
-            amount = 499.0,
+            amount = 349.0,
             currency = Currency.getInstance(Payments.CURRENCY_RUB)
         )
     )
